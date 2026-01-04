0c2e365754cadd5734238861aad057e83e77bbcb
Yaacoub Bakhouche <jacoubbakhouche@gmail.com>
2026-01-04 01:13:35 +0100

    Update styles and fix Java compatibility

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,8 +1,8 @@
 import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'com.foufou.app',
-  appName: 'foufou',
+  appId: 'com.layastyle.app',
+  appName: 'layaStyle',
   webDir: 'dist',
   server: {
     androidScheme: 'https'
